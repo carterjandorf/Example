de7215ad861c4b7a357bbee06b3ea3bee8c4bc07
carterjandorf <carter_jandorf@knights.ucf.edu>
2020-01-16 16:30:15 -0500

    Added a ! to the output message

--- Jandorf 01/src/jandorf_01/Jandorf01.java
+++ Jandorf 01/src/jandorf_01/Jandorf01.java
@@ -11,7 +11,7 @@ public class Jandorf01 {
 		System.out.println("Please enter your name: ");
 		String name = input.next();
 		
-		System.out.println("Hello, " + name);
+		System.out.println("Hello, " + name + "!");
 	}
 
 }
