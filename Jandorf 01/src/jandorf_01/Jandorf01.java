f89aecc6806c3b5c3a4f7d19cd7b7ac76ddba598
Carter <carter_jandorf@aol.com>
2020-01-16 16:17:42 -0500

    My first commit

---
+++ Jandorf 01/src/jandorf_01/Jandorf01.java
@@ -0,0 +1,17 @@
+package jandorf_01;
+
+import java.util.Scanner;
+
+public class Jandorf01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter your name: ");
+		String name = input.next();
+		
+		System.out.println("Hello, " + name);
+	}
+
+}
